0a174aebb6fb59b16496e80d4adce7762bf7d11e
ab625 <150213428+ab625@users.noreply.github.com>
2023-11-08 07:44:25 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "anirban@google.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+
+// accountId = 2
+accountEmail = "ab@ab.com"
+accountPassword = "212121"
+accountCity = "jaipur"
+console.log(accountId);
+console.table([accountId, accountEmail, accountCity, ])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+// alert( 3 + 3)
+console.log(3 + 3)
+let name = "Anirban"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to the power 53
+// bigint For greater Values
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+
+// object
+
+
+console.log(typeof "Anirban");
